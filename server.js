cbc2028bda6469e88d14a3112626d1a08a841940
Mark Vartolas <mark.vartolas@mail.utoronto.ca>
2020-11-26 11:33:50 -0500

    Ready for Heroku.

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 'use strict';
-const log = console.log
+const log = console.log;
 
-const express = require('express')
+const express = require('express');
 const app = express();
 
 const path = require('path');
 
 // Setting up a static directory for the files in /pub using Express middleware.
-app.use(express.static(path.join(__dirname, '/pub')))
+app.use(express.static(path.join(__dirname, '/pub')));
 
-const port = process.env.PORT || 5000
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'pub', 'example.html'));
+// });
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
 	log(`Listening on port ${port}...`)
-})
-
-app.get('/', (req, res) => {
-	// sending a string
-	//res.send('This should be the root route!')
-
-	//sending some HTML
-	res.send('<h1>This should be the root route!</h1>')
-})
+});

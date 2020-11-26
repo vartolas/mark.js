d1fc0a7eacb5cd5570f4f091a0770957cc9b841d
Mark Vartolas <mark.vartolas@mail.utoronto.ca>
2020-11-26 09:34:10 -0500

    Created node.js server.

---
+++ pub/js/example.js
@@ -0,0 +1,7 @@
+'use strict';
+const log = console.log;
+
+const mark = new Mark("#dateline");
+mark.setPosition("0", "1100px");
+mark.setColours("#DDDDDD", "yellow", "lime", "cyan");
+mark.setCurrentHighlighterColour(0);

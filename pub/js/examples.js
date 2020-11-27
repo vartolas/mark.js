a3682ca1d4831bb1769de88ea59f6c506f07994f
Mark Vartolas <mark.vartolas@mail.utoronto.ca>
2020-11-26 21:24:33 -0500

    Updated example webpage styling.

--- pub/js/examples.js
+++ pub/js/examples.js
@@ -4,5 +4,5 @@ const mark = new Mark("#datelineSeparator");
 mark.applyFixedPositioning(); // redundant because specifying a selector gives fixed positioning
 mark.setTop("220px");
 mark.setLeft("80px");
-mark.setColours("#FFFFFF", "yellow", "lime", "cyan");
+mark.setColours("#FFFFFF", "yellow", "lime", "#FED8B1");
 mark.setCurrentHighlighterColour(0); // redundant because we are setting the current colour to be the default colour

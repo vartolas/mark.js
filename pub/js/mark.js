e72614c31c888c15b00872e3991954ffecd6a6f5
Mark Vartolas <mark.vartolas@mail.utoronto.ca>
2020-12-14 23:34:48 -0500

    Implemented note deletion.

--- pub/js/mark.js
+++ pub/js/mark.js
@@ -537,27 +537,71 @@ function isChildOf(node, potentialParent) {
 }
 
 function leaveNote(markInstance, target, x, y) {
-  // Don't leave a note if the popUp was clicked, or if the noteTaking function is not on.
+  // Don't leave a note if the popUp was clicked, nor if the noteTaking function is not on.
   if (!markInstance.notetakerIsOn || isChildOf(target, markInstance.popUp)) {
     return;
   }
 
   const note = document.createElement("div");
   note.classList.add("note");
+  note.classList.add("maximizedNote");
   note.style.left = x + "px";
   note.style.top = y + "px";
 
-  // const deleteNoteBtn = document.createElement("div");
-  // deleteNoteBtn.classList.add("deleteNoteButton");
-  // deleteNoteBtn.addEventListener("click", note => {
-  //   markInstance.notes.splice(markInstance.notes.indexOf(note), 1);
-  //   markInstance.highlightsAndNotes.splice(markInstance.highlightsAndNotes.indexOf(note), 1);
-  //   log(note.parentElement, note.parentNode)
-  //   note.parentElement.removeChild(note);
-  // });
+  // Define functionality for deleting notes.
+  const deleteNoteBtn = document.createElement("div");
+  deleteNoteBtn.classList.add("noteButton");
+  deleteNoteBtn.classList.add("deleteNoteButton");
+  deleteNoteBtn.appendChild(document.createTextNode("✕"));
+  deleteNoteBtn.addEventListener("click", e => {
+    markInstance.notes.splice(markInstance.notes.indexOf(note), 1);
+    markInstance.highlightsAndNotes.splice(markInstance.highlightsAndNotes.indexOf(note), 1);
+    note.parentElement.removeChild(note);
+  });
 
-  note.appendChild(document.createTextNode(markInstance.noteTextarea.value));
-  // note.appendChild(deleteNoteBtn);
+  // Create the note content.
+  const noteContent = document.createElement("div");
+  noteContent.classList.add("noteContent");
+  noteContent.classList.add("maximizedNoteContent");
+  noteContent.appendChild(document.createTextNode(markInstance.noteTextarea.value));
+
+  // Define functionality for changing note view (maximizing/minimizing).
+  const changeNoteViewBtn = document.createElement("div");
+  changeNoteViewBtn.classList.add("noteButton");
+  changeNoteViewBtn.classList.add("changeNoteViewButton");
+  changeNoteViewBtn.classList.add("changeNoteViewButtonMaximized");
+  changeNoteViewBtn.appendChild(document.createTextNode("-"));
+  changeNoteViewBtn.addEventListener("click", e => {
+    if (changeNoteViewBtn.innerText == "-") {
+      // minimize the note's view
+      note.classList.remove("maximizedNote");
+      note.classList.add("minimizedNote");
+
+      noteContent.classList.remove("maximizedNoteContent");
+      noteContent.classList.add("minimizedNoteContent");
+
+      changeNoteViewBtn.removeChild(changeNoteViewBtn.childNodes[0]);
+      changeNoteViewBtn.appendChild(document.createTextNode("+"));
+      changeNoteViewBtn.classList.remove("changeNoteViewButtonMaximized");
+      changeNoteViewBtn.classList.add("changeNoteViewButtonMinimized");
+    } else {
+      // maximize the note's view
+      note.classList.remove("minimizedNote");
+      note.classList.add("maximizedNote");
+
+      noteContent.classList.remove("minimizedNoteContent");
+      noteContent.classList.add("maximizedNoteContent");
+
+      changeNoteViewBtn.removeChild(changeNoteViewBtn.childNodes[0]);
+      changeNoteViewBtn.appendChild(document.createTextNode("-"));
+      changeNoteViewBtn.classList.remove("changeNoteViewButtonMinimized");
+      changeNoteViewBtn.classList.add("changeNoteViewButtonMaximized");
+    }
+  });
+
+  note.appendChild(noteContent);
+  note.appendChild(changeNoteViewBtn);
+  note.appendChild(deleteNoteBtn);
 
   document.body.appendChild(note);
   markInstance.notes.push(note);

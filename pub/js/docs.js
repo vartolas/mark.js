2a15b672192462a1077590be2702ce3afa8e850e
Mark Vartolas <mark.vartolas@mail.utoronto.ca>
2020-12-19 21:32:50 -0500

    Finished last example.

--- pub/js/docs.js
+++ pub/js/docs.js
@@ -7,3 +7,20 @@ const apiLink = document.querySelector("#apiLink");
 apiLink.addEventListener("click", e => {
   $('body, html').animate({scrollTop:$("#apiHeader").offset().top - 70}, 1000)
 });
+
+const markInstance = new Mark();
+// colours that contrast well with whitish-yellow background
+markInstance.setHighlighterColours("orange", "lime", "pink", "cyan");
+
+// fix to top-right of viewport
+markInstance.setTop("80px");
+markInstance.setRight("20px");
+
+// initially minimize
+markInstance.minimize();
+
+// leave note for user to use the pop-up
+markInstance.addNote({
+  top: 100,
+  left: 100
+}, "Use the Mark.js pop-up located at the top-right of the screen to take notes!");
